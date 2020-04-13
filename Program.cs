3227e03849868555e98ab93978a25c224ac5aea1
Abhishek Kumar <ak@Abhisheks-MacBook-Pro.local>
2020-04-13 16:45:31 -0700

    Setup Hello World!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_x509_samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dotnet Core X509 Certificates samples
+A collection of samples to work with X509 certificates in dotnet core platform. 
